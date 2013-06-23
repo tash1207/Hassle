a517651422d11955a91a75b6a3987546c176362e
Tasha Hankewych <tash1207@bellsouth.net>
2013-06-23 14:33:35 -0400

    sending tweets to hassle works

--- src/co/tashawych/hassle/Hassle.java
+++ src/co/tashawych/hassle/Hassle.java
@@ -1,8 +1,15 @@
 package co.tashawych.hassle;
 
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -73,16 +80,58 @@ public class Hassle extends Activity {
 			Toast.makeText(this, "Please enter something to Hassle " + contact.name + " about.", 
 					Toast.LENGTH_SHORT).show();
 		}
+		else if (!text_on && !email_on && !twitter_on) {
+			Toast.makeText(this, "You need to enable some way of hassling " + contact.name + ".", 
+					Toast.LENGTH_SHORT).show();
+		}
 		else {
 			if (text_on) {
 				PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, Hassle.class), 0);
 				SmsManager sms = SmsManager.getDefault();
 			    sms.sendTextMessage(contact.phone, null, hassle, pi, null);
 			}
+			
+			if (twitter_on) {
+				SharedPreferences prefs = getSharedPreferences("Hassle", 0);
+				ConfigurationBuilder cb = new ConfigurationBuilder();
+                cb.setOAuthConsumerKey("R5F8PvM2k48BztKSs7pQ");
+                cb.setOAuthConsumerSecret("OZmt614YWtmRU3Hqh68R8X5PmDrUinRUnNn7ENZUwcI");
+                cb.setOAuthAccessToken(prefs.getString("twitter_cred_token", ""));
+                cb.setOAuthAccessTokenSecret(prefs.getString("twitter_cred_token_secret", ""));
+                
+                TwitterFactory tf = new TwitterFactory(cb.build());
+                Twitter twitter = tf.getInstance();
+                
+                // Check if the user's stored screen name begins with the @ symbol, if so remove it
+                if (contact.twitter.startsWith("@")) contact.twitter = contact.twitter.substring(1);
+                
+                new ComposeTweet(twitter, "@" + contact.twitter + " " + hassle).execute();
+			}
 		    
 			Toast.makeText(this, "Hassle: '" + hassle + "' ... sent!", 
 					Toast.LENGTH_SHORT).show();
 		}
 	}
+	
+    private class ComposeTweet extends AsyncTask<Void, Void, Boolean> {
+    	Twitter twitter;
+    	String tweet;
+    	public ComposeTweet(Twitter twitter, String tweet) {
+    		this.twitter = twitter;
+    		this.tweet = tweet;
+    	}
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+                try {
+                        StatusUpdate status = new StatusUpdate(tweet);
+                        twitter.updateStatus(status);
+                        return true;
+                } catch (TwitterException e) {
+                        e.printStackTrace();
+                }
+                return false;
+        }
+    }
 
 }
